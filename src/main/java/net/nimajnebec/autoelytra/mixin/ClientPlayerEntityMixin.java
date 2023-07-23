c264248c8c45ef67b58a996ad9d4ebf17e53f6ef
NimajnebEC <nimajnebec@users.noreply.github.com>
2023-07-23 18:43:18 +0100

    refactor: replace redirect with inject

--- src/main/java/net/nimajnebec/autoelytra/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/net/nimajnebec/autoelytra/mixin/ClientPlayerEntityMixin.java
@@ -10,16 +10,16 @@ import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.screen.slot.SlotActionType;
 import net.minecraft.util.collection.DefaultedList;
-import net.nimajnebec.autoelytra.AutoElytra;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,42 +27,39 @@ import java.util.List;
 @Mixin(targets = "net.minecraft.client.network.ClientPlayerEntity")
 public abstract class ClientPlayerEntityMixin extends AbstractClientPlayerEntity {
     @Unique private static final int chestSlot = EquipmentSlot.CHEST.getOffsetEntitySlotId(PlayerInventory.MAIN_SIZE);
-    @Unique @Nullable private ItemStack previousChestItem;
+    @Unique private NbtCompound previousChestItemNbt;
     @Shadow @Final protected MinecraftClient client;
 
     public ClientPlayerEntityMixin(ClientWorld world, GameProfile profile) {
         super(world, profile);
     }
 
-    @Redirect(method = "tickMovement()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;getEquippedStack(Lnet/minecraft/entity/EquipmentSlot;)Lnet/minecraft/item/ItemStack;"))
-    private ItemStack checkWearingElytra(ClientPlayerEntity instance, EquipmentSlot equipmentSlot) {
-        if (this.canStartFlying()) tryEquipElytra();
-        return this.getEquippedStack(equipmentSlot);
+    @Inject(method = "tickMovement()V", at = @At(value = "INVOKE", shift = At.Shift.BEFORE, target = "Lnet/minecraft/client/network/ClientPlayerEntity;getEquippedStack(Lnet/minecraft/entity/EquipmentSlot;)Lnet/minecraft/item/ItemStack;"))
+    private void tryEquipElytra(CallbackInfo ci) {
+        if (this.canStartFlying()) {
+            List<ItemStack> inventory = this.getCombinedInventory();
+
+            // Return if elytra is already equipped
+            if (inventory.get(chestSlot).isOf(Items.ELYTRA)) return;
+
+            // Find elytra in inventory
+            for (int slot = 0; slot < inventory.size(); slot++) {
+                ItemStack stack = inventory.get(slot);
+
+                if (stack.isOf(Items.ELYTRA)) {
+                    this.previousChestItemNbt = inventory.get(chestSlot).getOrCreateNbt();
+                    this.swapSlots(slot, chestSlot);
+                    return;
+                }
+            }
+        }
     }
 
     @Unique private boolean canStartFlying() {
         return !this.isOnGround() && !this.isFallFlying() && !this.isTouchingWater() && !this.hasStatusEffect(StatusEffects.LEVITATION);
     }
 
-    @Unique private void tryEquipElytra() {
-        List<ItemStack> inventory = this.getCombinedInventory();
-
-        // Return if elytra is alredy equiped
-        if (inventory.get(this.chestSlot).isOf(Items.ELYTRA)) return;
-
-        // Find elytra in inventory
-        for (int slot = 0; slot < inventory.size(); slot++) {
-            ItemStack stack = inventory.get(slot);
-
-            if (stack.isOf(Items.ELYTRA)) {
-                this.previousChestItem = inventory.get(chestSlot);
-                this.swapSlots(slot, chestSlot);
-                return;
-            }
-        }
-    }
-
-    @Unique private void swapSlots(int slotA, int slotB) {
+    @Unique public void swapSlots(int slotA, int slotB) {
 
         // Convert inventory slot to screen slot
         int slotAScreen = -1;
@@ -78,7 +75,7 @@ public abstract class ClientPlayerEntityMixin extends AbstractClientPlayerEntity
         client.interactionManager.clickSlot(this.playerScreenHandler.syncId, slotAScreen, slotB, SlotActionType.SWAP, this);
     }
 
-    @Unique private List<ItemStack> getCombinedInventory() {
+    @Unique public List<ItemStack> getCombinedInventory() {
         PlayerInventory inventory = this.getInventory();
         List<ItemStack> result = new ArrayList<>();
 
