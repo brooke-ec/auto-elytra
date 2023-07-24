d436d4a8c3fc18242d6725ab31109837fb113ed7
NimajnebEC <nimajnebec@users.noreply.github.com>
2023-07-24 18:22:27 +0100

    feat: add installed message to initialize

--- src/main/java/net/nimajnebec/autoelytra/AutoElytra.java
+++ src/main/java/net/nimajnebec/autoelytra/AutoElytra.java
@@ -1,10 +1,15 @@
 package net.nimajnebec.autoelytra;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
+import net.fabricmc.loader.api.metadata.ModMetadata;
 import net.minecraft.nbt.NbtCompound;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Optional;
 
 public class AutoElytra implements ClientModInitializer {
     public static final Logger logger = LogManager.getLogger();
@@ -17,7 +22,13 @@ public class AutoElytra implements ClientModInitializer {
     }
 
     @Override
-    public void onInitializeClient() {}
+    public void onInitializeClient() {
+        Optional<ModContainer> modContainer = FabricLoader.getInstance().getModContainer("autoelytra");
+        if (modContainer.isPresent()) {
+            ModMetadata metadata = modContainer.get().getMetadata();
+            logger.info("Auto Elytra v%s installed!".formatted(metadata.getVersion()));
+        }
+    }
 
     public void setPreviousChestItemNbt(@Nullable NbtCompound value) {
         this.previousChestItemNbt = value;
